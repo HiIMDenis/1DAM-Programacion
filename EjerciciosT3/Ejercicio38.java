e60d363f5ddb0cef056aa7f4d7ed950dceba2f51
Denis Stupak <114920082+HiIMDenis@users.noreply.github.com>
2022-11-29 16:42:27 +0100

    Update Ejercicio38.java

--- EjerciciosT3/Ejercicio38.java
+++ EjerciciosT3/Ejercicio38.java
@@ -15,10 +15,10 @@ public class Ejercicio38 {
         double r1, r2;
         int a = 1, b = -5, c =6;
         
-        double Discriminante;
-        Discriminante = Math.sqrt(Math.pow(b, 2)-4*a*c);
+        double Dis;
+        Dis = Math.sqrt(Math.pow(b, 2)-4*a*c);
         
-        if(Discriminante > 0){
+        if(Dis > 0){
             System.out.println("Tiene dos soluciones:\n");
             
             r1= (-1*b + Math.sqrt(Math.pow(b, 2)-4*a*c))/2.0*a;
@@ -28,7 +28,7 @@ public class Ejercicio38 {
             System.out.println("Solucion 2 = "+r2);
             
         }else{
-               if(Discriminante == 0){
+               if(Dis == 0){
                   System.out.println("Tiene una solucion:\n");
                 
                   r1= (-1*b + Math.sqrt(Math.pow(b, 2)-4*a*c))/2.0*a;
