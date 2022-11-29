cb2420d0c8800045bd43723fcf307d5122c4d757
Denis Stupak <114920082+HiIMDenis@users.noreply.github.com>
2022-11-29 16:55:06 +0100

    Add files via upload

---
+++ EjerciciosT3/Ejercicio43.java
@@ -0,0 +1,31 @@
+package ejercicio43;
+
+/**
+ *
+ * @author Denis <denis.stupak23@gmai.com>
+ */
+public class Ejercicio43 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        double pi; 
+ 
+        pi = 4 * Math.atan(1); 
+ 
+        System.out.printf("Valor de pi: %.4f \n", pi);
+        
+
+
+
+
+
+
+
+
+
+    }
+
+}
