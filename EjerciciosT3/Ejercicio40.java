15c6c65f2801a5be042d0c493a90f3ee74f4f4e6
Denis Stupak <114920082+HiIMDenis@users.noreply.github.com>
2022-11-29 16:40:56 +0100

    Update Ejercicio40.java

--- EjerciciosT3/Ejercicio40.java
+++ EjerciciosT3/Ejercicio40.java
@@ -12,7 +12,7 @@ public class Ejercicio40 {
     public static void main(String[] args) {
        
     System.out.println("Nucleos: "+Runtime.getRuntime().availableProcessors()); 
-    System.out.println("Cantidad de memoria total al proceso de Java: "
+    System.out.println("Cantidad de memoria total: "
                 +Runtime.getRuntime().totalMemory()+" bytes");
 
     }
