89839787d2bae03c6bf6cb198c5e5eac5f13dbdc
Denis Stupak <114920082+HiIMDenis@users.noreply.github.com>
2022-12-05 20:57:38 +0100

    Add files via upload

---
+++ 02-EjerciciosT2/Ejercicio53.java
@@ -0,0 +1,50 @@
+package ejercicio53;
+import java.awt.Color;
+import java.util.*;
+/**
+ *
+ * @author Denis <denis.stupak23@gmai.com>
+ */
+public class Ejercicio53 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    // Creamos un objeto TreeMap para almacenar las asociaciones
+    // entre matrículas y colores de coches
+    Map<String, Color> coches = new TreeMap<>();
+
+    // Añadimos algunas asociaciones a nuestro objeto TreeMap
+    coches.put("12535ABC", Color.RED);
+    coches.put("98525KWX", Color.YELLOW);
+    coches.put("17632MSE", Color.GREEN);
+    coches.put("85321ABC", Color.YELLOW);
+
+    // Mostramos el tamaño del map
+    System.out.println("Tamaño del map: "+coches.size());
+
+    // Mostramos el contenido del map utilizando su método toString()
+    System.out.println("Contenido del map: "+coches);
+    
+    // Pedimos al usuario que introduzca la matrícula de un coche
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Introduce la matrícula de un coche: ");
+    String matricula = scanner.nextLine();
+
+    // Comprobamos si la matrícula introducida existe en el map
+    if (coches.containsKey(matricula)) {
+      // Si existe, mostramos el color del coche
+      System.out.println("El color del coche con matricula "+matricula+
+              " es " + coches.get(matricula));
+    } else {
+      // Si no existe, mostramos un mensaje
+      System.out.println("No existe esa matricula");
+    }
+    
+    //Treemap lo que hace es ordena los objetos con un orden mientras que el 
+    //hasmap no mantiene un orden.
+    }
+
+}
